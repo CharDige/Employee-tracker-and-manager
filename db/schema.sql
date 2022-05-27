5aeeb6611c5101ed28d46ccd692c5e4d618b3a4c
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-05-27 15:39:51 +0930

    Added CREATE TABLE for department, role and employee

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,35 @@ CREATE DATABASE workplace_db;
 
 -- Use the database --
 USE workplace_db;
+
+-- Create department table --
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+-- Create role table --
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+-- Create employee table --
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
