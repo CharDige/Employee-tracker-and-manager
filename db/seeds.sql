f77ce27ae6f13756891aefb7ed9041c9a2da0c39
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-05-31 11:19:07 +0930

    Added more seed data to utilise more managers in manager column

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,7 +15,12 @@ VALUES  ("Salesperson", 55000, 1),
         ("Finance Team Lead", 120000, 4),
         ("Admin Team Lead", 85000, 5),
         ("Legal Team Lead", 180000, 6),
-        ("Sales Team Lead", 75000, 1);
+        ("Sales Team Lead", 75000, 1),
+        ("Communications Officer", 85000, 2),
+        ("Software Engineer", 100000, 3),
+        ("Business Support Officer", 95000, 4),
+        ("Admin Officer", 55000, 5),
+        ("Legal Adviser", 150000, 6);
 
 -- Insert data into employee table --
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
@@ -25,4 +30,9 @@ VALUES  ("Harry", "Amell", 2, null),
         ("Dmitri", "Mahariel", 5, null),
         ("Damon", "Aeducan", 6, null),
         ("Ella", "Brosca", 7, null),
-        ("Natalie", "Cousland", 1, 6);
+        ("Natalie", "Cousland", 1, 6),
+        ("Amelia", "Trevelyan", 8, 1),
+        ("Daniel", "Lavellan", 9, 2),
+        ("Hannah", "Cadash", 10, 3),
+        ("Lucy", "Adaar", 11, 4),
+        ("Jeremy", "Hawke", 12, 5);
